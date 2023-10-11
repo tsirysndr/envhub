fddba51f64f2638878e5c190a881702970f218c4
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-11 07:57:07 +0000

    bump version code

--- flake.nix
+++ flake.nix
@@ -40,7 +40,7 @@
           inherit src;
           
           pname = "envhub";
-          version = "0.2.0";
+          version = "0.2.1";
           cargoExtraArgs = "--package=envhub";
 
           buildInputs = [
